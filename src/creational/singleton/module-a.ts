c933e8d7dbb7331b63c59ae87fd50b99d7094907
“Brendo” <brendo.gomes@educbank.com.br>
2025-01-04 15:13:49 -0300

    remove files

--- src/creational/singleton/module-a.ts
+++
@@ -1,8 +0,0 @@
-import { DatabaseClassic } from './db/database-classic';
-
-const myDatabaseClassic = DatabaseClassic.getInstance();
-myDatabaseClassic.addUser({ name: 'João', age: 32 });
-myDatabaseClassic.addUser({ name: 'Maria', age: 25 });
-myDatabaseClassic.addUser({ name: 'Carlos', age: 29 });
-
-export { myDatabaseClassic };
